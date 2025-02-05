27ea39f45c5ccd2675d7f9cda842c3f0e051b2b9
Gabby <gabbie10112005@gmail.com>
2025-02-05 09:45:04 -0600

    Initial commit

---
+++ Queues_Miller_Gabriella/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Queues_Miller_Gabriella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IM GONNA FREAKING KILL MYSELF WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }
+    }
+}
